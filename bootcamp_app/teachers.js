8acbcd13cd78f5b8389c28d76a3c6b16ca421c17
hugo-pb <h.palomerabecerra@gmail.com>
2022-09-21 21:22:29 -0600

    query hack prevention

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,16 +6,19 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+const cohort = process.argv[2];
+const value = [`%${cohort}%` ||'JUL02' ]
 
-pool.query(`
+const q = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name like $1 
 ORDER BY teacher;
-`)
+`; 
+pool.query( q ,value )
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
